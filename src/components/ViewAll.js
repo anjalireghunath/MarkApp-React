9166408578c14573a5c381e30b17a15abf71f6ac
anjalireghunath <anjalireghu98@gmail.com>
2022-05-26 12:16:08 +0530

    add view

--- src/components/ViewAll.js
+++ src/components/ViewAll.js
@@ -34,8 +34,8 @@ const ViewAll = () => {
             <div className='col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12'>
             <div className='row g-3'>
                 <div className='col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12'>
-                <table class="table table-primary table-striped">
-  <thead>
+                <table className="table table-primary table-striped">
+  <thead className="table-danger">
     <tr>
       <th scope="col">Name</th>
       <th scope="col">AdmNo</th>
